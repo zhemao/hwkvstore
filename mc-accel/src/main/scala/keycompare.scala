348a08f0c70504f53120bdc58528ac9fd695a74d
Howard Mao <zhehao.mao@gmail.com>
2014-10-21 10:09:06 -0700

    delay keycompare memory by one cycle

--- mc-accel/src/main/scala/keycompare.scala
+++ mc-accel/src/main/scala/keycompare.scala
@@ -21,16 +21,18 @@ class KeyCompare(KeyAddrSize: Int, HashSize: Int, WordSize: Int) extends Module 
   val checkFirst = Reg(Bool())
   val curHash = Mux(checkFirst, curInfo.hash1, curInfo.hash2)
 
+  // the addresses will be delayed by one going out
+  // first data is word is at index 1, since index 0 is for length
   io.curKeyAddr := index - UInt(1)
   io.allKeyAddr := Cat(curHash, index)
 
   val wordLen = curInfo.len(KeyLenSize - 1, WordShift)
   val byteOff = curInfo.len(WordShift - 1, 0)
   val reachedEnd = Mux(byteOff === UInt(0),
-    index === wordLen, index === wordLen + UInt(1))
+    index === wordLen + UInt(1), index === wordLen + UInt(2))
 
-  val (s_wait :: s_check_len :: s_check_data :: s_handoff :: Nil) =
-    Enum(UInt(), 4)
+  val (s_wait :: s_delay_len :: s_check_len ::
+    s_delay_data :: s_check_data :: s_handoff :: Nil) = Enum(UInt(), 6)
   val state = Reg(init = s_wait)
 
   val hashFound = Reg(Bool())
@@ -46,26 +48,32 @@ class KeyCompare(KeyAddrSize: Int, HashSize: Int, WordSize: Int) extends Module 
         curInfo := io.hashIn.bits
         checkFirst := Bool(true)
         index := UInt(0)
-        state := s_check_len
+        state := s_delay_len
         hashFound := Bool(false)
       }
     }
+    is (s_delay_len) {
+      state := s_check_len
+    }
     is (s_check_len) {
       when (io.allKeyData(KeyAddrSize - 1, 0) === curInfo.len) {
-        state := s_check_data
+        state := s_delay_data
         index := UInt(1)
       } .elsewhen (checkFirst) {
         checkFirst := Bool(false)
-        state := s_check_len
+        state := s_delay_len
       } .otherwise {
         state := s_handoff
       }
+    }
+    is (s_delay_data) {
+      state := s_check_data
     }
     is (s_check_data) {
       when (io.curKeyData != io.allKeyData) {
         when (checkFirst) {
           checkFirst := Bool(false)
-          state := s_check_len
+          state := s_delay_len
           index := UInt(0)
         } .otherwise {
           state := s_handoff
@@ -111,8 +119,6 @@ class KeyCompareSetup(val MaxKeySize: Int, val NumKeys: Int, val WordSize: Int)
   val allKeyMem = Mem(UInt(width = WordSize), NumKeys * MaxKeySize)
 
   val keycomp = Module(new KeyCompare(KeyAddrSize, HashSize, WordSize))
-  keycomp.io.curKeyData := curKeyMem(keycomp.io.curKeyAddr)
-  keycomp.io.allKeyData := allKeyMem(keycomp.io.allKeyAddr)
   keycomp.io.hashIn.bits.hash1 := io.hash1
   keycomp.io.hashIn.bits.hash2 := io.hash2
   keycomp.io.hashIn.bits.len := io.len
@@ -122,6 +128,12 @@ class KeyCompareSetup(val MaxKeySize: Int, val NumKeys: Int, val WordSize: Int)
   io.done := keycomp.io.hashOut.valid
   io.hashsel := keycomp.io.hashOut.bits.hash
   io.found := keycomp.io.hashOut.bits.found
+
+  val curKeyAddr = Reg(next = keycomp.io.curKeyAddr)
+  keycomp.io.curKeyData := curKeyMem(curKeyAddr)
+
+  val allKeyAddr = Reg(next = keycomp.io.allKeyAddr)
+  keycomp.io.allKeyData := allKeyMem(allKeyAddr)
 
   when (io.curKeyWrite) {
     curKeyMem(io.curKeyAddr) := io.curKeyData
@@ -134,97 +146,88 @@ class KeyCompareSetup(val MaxKeySize: Int, val NumKeys: Int, val WordSize: Int)
 
 class KeyCompareTest(c: KeyCompareSetup) extends Tester(c) {
   val key1 = "abcdefghijklmnopqrstuvwxyz"
-  val key2 = "abcdegfhijklmnopqrstuvwxyz"
+  val key2 = "abcdefghijklmnopqrstuvwxzy"
   val key3 = "0123456789"
 
   val WordBytes = c.WordSize / 8
-  val keyWords1 = messToWords(key1, WordBytes)
-  val keyWords2 = messToWords(key2, WordBytes)
-  val keyWords3 = messToWords(key3, WordBytes)
 
-  poke(c.io.curKeyWrite, 1)
-  poke(c.io.allKeyWrite, 1)
-  poke(c.io.allKeyAddr, 0)
-  poke(c.io.allKeyData, key1.length)
-  step(1)
-  for (i <- 0 until keyWords1.length) {
-    poke(c.io.allKeyAddr, i + 1)
-    poke(c.io.allKeyData, keyWords1(i))
-    poke(c.io.curKeyAddr, i)
-    poke(c.io.curKeyData, keyWords1(i))
-    step(1)
+  def writeCurKey(key: String) {
+    val keyWords = messToWords(key, WordBytes)
+    poke(c.io.curKeyWrite, 1)
+    for (i <- 0 until keyWords.length) {
+      poke(c.io.curKeyAddr, i)
+      poke(c.io.curKeyData, keyWords(i))
+      step(1)
+    }
+    poke(c.io.curKeyWrite, 0)
   }
-  poke(c.io.curKeyWrite, 0)
-  poke(c.io.allKeyAddr, c.MaxKeySize)
-  poke(c.io.allKeyData, key2.length)
-  step(1)
-  for (i <- 0 until keyWords2.length) {
-    poke(c.io.allKeyAddr, c.MaxKeySize + i + 1)
-    poke(c.io.allKeyData, keyWords2(i))
+
+  def writeKeyData(start: Int, key: String) {
+    val keyWords = messToWords(key, WordBytes)
+    poke(c.io.allKeyWrite, 1)
+    poke(c.io.allKeyAddr, start)
+    poke(c.io.allKeyData, key.length)
     step(1)
+    for (i <- 0 until keyWords.length) {
+      poke(c.io.allKeyAddr, start + i + 1)
+      poke(c.io.allKeyData, keyWords(i))
+      step(1)
+    }
+    poke(c.io.allKeyWrite, 0)
   }
-  poke(c.io.allKeyAddr, 2 * c.MaxKeySize)
-  poke(c.io.allKeyData, key3.length)
-  step(1)
-  for (i <- 0 until keyWords3.length) {
-    poke(c.io.allKeyAddr, 2 * c.MaxKeySize + i + 1)
-    poke(c.io.allKeyData, keyWords3(i))
+
+  def waitForDone {
+    isTrace = false
+    while (peek(c.io.done) == 0)
+      step(1)
+    isTrace = true
+  }
+
+  def checkHashes(hash1: Int, hash2: Int, expectation: Int) {
+    expect(c.io.ready, 1)
+
+    poke(c.io.hash1, hash1)
+    poke(c.io.hash2, hash2)
+    poke(c.io.start, 1)
     step(1)
+    poke(c.io.start, 0)
+
+    waitForDone
+
+    expectation match {
+      case 0 => {
+        expect(c.io.found, 1)
+        expect(c.io.hashsel, hash1)
+      }
+      case 1 => {
+        expect(c.io.found, 1)
+        expect(c.io.hashsel, hash2)
+      }
+      case 2 => {
+        expect(c.io.found, 0)
+      }
+    }
+
+    poke(c.io.finish, 1)
+    step(1)
+    poke(c.io.finish, 0)
   }
-  poke(c.io.allKeyWrite, 0)
+
+  writeCurKey(key1)
+  writeKeyData(0, key1)
+  writeKeyData(c.MaxKeySize, key2)
+  writeKeyData(2 * c.MaxKeySize, key3)
+
+  poke(c.io.len, key1.length)
 
   // check that we find the answer when hash1 is 0
-  expect(c.io.ready, 1)
-  poke(c.io.hash1, 0)
-  poke(c.io.hash2, 1)
-  poke(c.io.len, key1.length)
-  poke(c.io.start, 1)
-  step(1)
-  poke(c.io.start, 0)
-
-  while (peek(c.io.done) == 0)
-    step(1)
-
-  expect(c.io.hashsel, 0)
-  expect(c.io.found, 1)
-  poke(c.io.finish, 1)
-  step(1)
-  expect(c.io.ready, 1)
-  poke(c.io.finish, 0)
-  step(1)
+  checkHashes(0, 1, 0)
 
   // check that we find the answer when hash2 is 0
-  poke(c.io.hash1, 1)
-  poke(c.io.hash2, 0)
-  poke(c.io.start, 1)
-  step(1)
-  poke(c.io.start, 0)
-
-  while (peek(c.io.done) == 0)
-    step(1)
-
-  expect(c.io.found, 1)
-  expect(c.io.hashsel, 0)
-  poke(c.io.finish, 1)
-  step(1)
-  poke(c.io.finish, 0)
-  step(1)
+  checkHashes(1, 0, 1)
 
   // check that we find no answer when neither of the hashes is 0
-  poke(c.io.hash1, 1)
-  poke(c.io.hash2, 2)
-  poke(c.io.start, 1)
-  step(1)
-  poke(c.io.start, 0)
-
-  while (peek(c.io.done) == 0)
-    step(1)
-
-  expect(c.io.found, 0)
-  poke(c.io.finish, 1)
-  step(1)
-  poke(c.io.finish, 0)
-  step(1)
+  checkHashes(1, 2, 2)
 }
 
 object KeyCompareMain {
