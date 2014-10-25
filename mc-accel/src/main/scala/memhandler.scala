27dad2467688b417f74c953936c2f76100735a41
Howard Mao <zhehao.mao@gmail.com>
2014-10-25 10:52:08 -0700

    complete memory handler

--- mc-accel/src/main/scala/memhandler.scala
+++ mc-accel/src/main/scala/memhandler.scala
@@ -5,7 +5,7 @@ import Chisel.AdvTester._
 import McAccel.Constants._
 import McAccel.TestUtils._
 
-class MemoryHandler(WordSize: Int, ValAddrSize: Int, KeyAddrSize: Int)
+class MemoryHandler(val WordSize: Int, ValAddrSize: Int, KeyAddrSize: Int)
       extends Module with CoreParameters with MemoryOpConstants {
   val io = new Bundle {
     val mem = new HellaCacheIO
@@ -22,13 +22,17 @@ class MemoryHandler(WordSize: Int, ValAddrSize: Int, KeyAddrSize: Int)
     val cmd = Decoupled(new MemCommand(coreDataBits, ValAddrSize, 2)).flip
   }
 
-  val BytesPerWord = coreDataBits / 8
-  val ByteShift = log2Up(BytesPerWord)
+  val BytesPerInputWord = coreDataBits / 8
+  val InputByteShift = log2Up(BytesPerInputWord)
+  val BytesPerOutputWord = WordSize / 8
+  val OutputByteShift = log2Up(BytesPerOutputWord)
+  val OutputWordsPerInputWord = coreDataBits / WordSize
 
-  val (s_idle :: s_stream_key_wait :: s_stream_key_out ::
-    s_req_data :: s_proc_resp :: s_finish :: Nil) = Enum(Bits(), 6)
+  val (s_idle :: s_stream_key_wait :: s_shift_bytes :: s_shift_words ::
+    s_req_data :: s_proc_resp :: s_finish :: Nil) = Enum(Bits(), 7)
   val state = Reg(init = s_idle)
-  val ret_state = Reg(init = s_idle)
+  val outputMask = Reg(Bits(width = 3))
+  val shiftByWord = Reg(Bool())
 
   val len = Reg(UInt(width = ValAddrSize))
   val bytesread = Reg(UInt(width = ValAddrSize))
@@ -39,6 +43,9 @@ class MemoryHandler(WordSize: Int, ValAddrSize: Int, KeyAddrSize: Int)
   val keyDataBits = Reg(UInt(width = 8))
 
   val word = Reg(Bits(width = coreDataBits))
+  val byteOff = bytesread(InputByteShift - 1, 0)
+  val wordsread = bytesread(ValAddrSize - 1, OutputByteShift)
+  val wordlen = len(ValAddrSize - 1, OutputByteShift)
 
   io.mem.req.valid := (state === s_req_data)
   io.mem.req.bits.addr := readaddr
@@ -47,8 +54,18 @@ class MemoryHandler(WordSize: Int, ValAddrSize: Int, KeyAddrSize: Int)
   io.mem.req.bits.cmd := M_XRD
   io.mem.req.bits.phys := Bool(true)
 
-  io.keyData.valid := (state === s_stream_key_out)
+  val shifting = (state === s_shift_bytes) || (state === s_shift_words)
+
+  io.keyData.valid := shifting && outputMask(0).toBool
   io.keyData.bits  := word(7, 0)
+
+  io.allKeyAddr := writeaddr(KeyAddrSize + OutputByteShift - 1, OutputByteShift)
+  io.allKeyData := word(WordSize - 1, 0)
+  io.allKeyWrite := shifting && outputMask(1).toBool
+
+  io.cacheWriteData := word(7, 0)
+  io.cacheWriteAddr := writeaddr
+  io.cacheWriteEn := shifting && outputMask(2).toBool
 
   io.cmd.ready := (state === s_idle)
 
@@ -63,6 +80,18 @@ class MemoryHandler(WordSize: Int, ValAddrSize: Int, KeyAddrSize: Int)
         switch (io.cmd.bits.action) {
           is (StreamKeyAction) {
             state := s_stream_key_wait
+            shiftByWord := Bool(false)
+            outputMask := Bits("b001")
+          }
+          is (CopyKeyAction) {
+            state := s_req_data
+            shiftByWord := Bool(true)
+            outputMask := Bits("b010")
+          }
+          is (CopyValueAction) {
+            state := s_req_data
+            shiftByWord := Bool(false)
+            outputMask := Bits("b100")
           }
         }
       }
@@ -70,29 +99,52 @@ class MemoryHandler(WordSize: Int, ValAddrSize: Int, KeyAddrSize: Int)
     is (s_stream_key_wait) {
       when (io.keyData.ready) {
         state := s_req_data
-        ret_state := s_stream_key_out
       }
     }
-    is (s_stream_key_out) {
+    is (s_shift_bytes) {
       bytesread := bytesread + UInt(1)
+      writeaddr := writeaddr + UInt(1)
       word := Cat(UInt(0, 8), word(coreDataBits - 1, 8))
       when (bytesread === len - UInt(1)) {
         state := s_finish
-      } .elsewhen (bytesread(ByteShift - 1, 0) === UInt(BytesPerWord - 1)) {
-        ret_state := s_stream_key_out
+      } .elsewhen (byteOff === UInt(BytesPerInputWord - 1)) {
         state := s_req_data
+      }
+    }
+    is (s_shift_words) {
+      bytesread := bytesread + UInt(BytesPerOutputWord)
+      if (WordSize == coreDataBits) {
+        when (wordsread === wordlen - UInt(1)) {
+          state := s_finish
+        } .otherwise {
+          state := s_req_data
+          writeaddr := writeaddr + UInt(BytesPerOutputWord)
+        }
+      } else {
+        word := Cat(UInt(0, WordSize), word(coreDataBits - 1, WordSize))
+        val wordOff = bytesread(InputByteShift - 1, OutputByteShift)
+        when (wordsread === wordlen - UInt(1)) {
+          state := s_finish
+        } .elsewhen (wordOff === UInt(OutputWordsPerInputWord - 1)) {
+          state := s_req_data
+          writeaddr := writeaddr + UInt(BytesPerOutputWord)
+        }
       }
     }
     is (s_req_data) {
       when (io.mem.req.ready) {
-        readaddr := readaddr + UInt(BytesPerWord)
+        readaddr := readaddr + UInt(BytesPerInputWord)
         state := s_proc_resp
       }
     }
     is (s_proc_resp) {
       when (io.mem.resp.valid) {
         word := io.mem.resp.bits.data
-        state := ret_state
+        when (shiftByWord) {
+          state := s_shift_words
+        } .otherwise {
+          state := s_shift_bytes
+        }
       }
     }
     is (s_finish) {
@@ -109,30 +161,74 @@ class MemoryHandlerTest(c: MemoryHandler) extends AdvTester(c) {
     c.io.mem.resp,
     (sckt: HellaCacheResp, in: TestMemResp) => in.inject(this, sckt))
 
-  val randomMemory = new RandomMemory(64, 64*64*2,
+  val memory = new RandomMemory(64, 64*64*2,
     MemReq_OHandler.outputs, MemResp_IHandler.inputs)
 
   val key = "asdkfjqwekjfasdkfj"
-  val keyWords = messToWords(key, 8)
+  val keyInWords = messToWords(key, 8)
+  val keyOutWords = messToWords(key, c.WordSize / 8)
 
-  randomMemory.store_data(0, keyWords)
+  memory.store_data(0, keyInWords)
 
   wire_poke(c.io.cmd.valid, 1)
   wire_poke(c.io.cmd.bits.action, 0)
   wire_poke(c.io.cmd.bits.readstart, 0)
+  wire_poke(c.io.cmd.bits.writestart, 0)
   wire_poke(c.io.cmd.bits.len, key.length)
-  step(1)
+  takestep()
   wire_poke(c.io.cmd.valid, 0)
   wire_poke(c.io.keyData.ready, 1)
 
   var addr = 0
 
   until (peek(c.io.cmd.ready) == 1, 450) {
+    expect(c.io.cacheWriteEn, 0)
+    expect(c.io.allKeyWrite, 0)
     if (peek(c.io.keyData.valid) == 1) {
+      isTrace = true
       expect(c.io.keyData.bits, key(addr))
       addr += 1
+      isTrace = false
     }
-    randomMemory.process()
+    memory.process()
+  }
+
+  wire_poke(c.io.keyData.ready, 0)
+
+  wire_poke(c.io.cmd.valid, 1)
+  wire_poke(c.io.cmd.bits.action, 1)
+  takestep()
+  wire_poke(c.io.cmd.valid, 0)
+
+  memory.store_data(0, keyInWords)
+
+  until (peek(c.io.cmd.ready) == 1, 450) {
+    expect(c.io.keyData.valid, 0)
+    expect(c.io.cacheWriteEn, 0)
+    if (peek(c.io.allKeyWrite) == 1) {
+      isTrace = true
+      addr = peek(c.io.allKeyAddr).toInt
+      expect(c.io.allKeyData, keyOutWords(addr))
+      isTrace = false
+    }
+    memory.process()
+  }
+
+  wire_poke(c.io.cmd.valid, 1)
+  wire_poke(c.io.cmd.bits.action, 2)
+  takestep()
+  wire_poke(c.io.cmd.valid, 0)
+
+  until (peek(c.io.cmd.ready) == 1, 450) {
+    expect(c.io.keyData.valid, 0)
+    expect(c.io.allKeyWrite, 0)
+    if (peek(c.io.cacheWriteEn) == 1) {
+      isTrace = true
+      addr = peek(c.io.cacheWriteAddr).toInt
+      expect(c.io.cacheWriteData, key(addr))
+      isTrace = false
+    }
+    memory.process()
   }
 }
 
