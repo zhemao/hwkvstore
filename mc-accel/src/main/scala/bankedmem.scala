32740bc7887856a2b3b6e624b9a2101516aec238
Howard Mao <zhehao.mao@gmail.com>
2014-11-06 18:09:45 -0800

    bankedmem should split addresses before registering

--- mc-accel/src/main/scala/bankedmem.scala
+++ mc-accel/src/main/scala/bankedmem.scala
@@ -19,37 +19,40 @@ class BankedMem(val WordSize: Int, val BankSize: Int, val NumBanks: Int)
     val writeEn   = Bool(INPUT)
   }
 
-  val readAddrReg  = Reg(next = io.readAddr)
-  val writeAddrReg = Reg(next = io.writeAddr)
+  val bankReadAddr0 = io.readAddr(BankAddrSize - 1, 0)
+  val bankReadSel0  = io.readAddr(FullAddrSize - 1, BankAddrSize)
+  val bankReadAddr1 = Reg(next = bankReadAddr0)
+  val bankReadSel1  = Reg(next = bankReadSel0)
+  val bankReadSel2  = Reg(next = bankReadSel1)
+
+  val bankWriteAddr0 = io.writeAddr(BankAddrSize - 1, 0)
+  val bankWriteSel0  = io.writeAddr(FullAddrSize - 1, BankAddrSize)
+  val bankWriteAddr1 = Reg(next = bankWriteAddr0)
+  val bankWriteSel1  = Reg(next = bankWriteSel0)
+
   val writeDataReg = Reg(next = io.writeData)
   val writeEnReg   = Reg(next = io.writeEn)
 
-  val bankReadAddr0 = readAddrReg(BankAddrSize - 1, 0)
-  val bankReadSel0  = readAddrReg(FullAddrSize - 1, BankAddrSize)
-  val bankReadSel1  = Reg(next = bankReadSel0)
-
-  val bankWriteAddr = writeAddrReg(BankAddrSize - 1, 0)
-  val bankWriteSel  = writeAddrReg(FullAddrSize - 1, BankAddrSize)
-  val bankWriteEn   = Vec((0 until NumBanks).map {
-    i => writeEnReg && bankWriteSel === UInt(i)
+  val bankWriteEn = Vec((0 until NumBanks).map {
+    i => writeEnReg && bankWriteSel1 === UInt(i)
   })
 
   val banks = Array.fill(NumBanks) {
     Mem(UInt(width = WordSize), BankSize, true)
   }
-  val bankReadData1 = Vec.fill(NumBanks) { Reg(UInt(width = WordSize)) }
+  val bankReadData2 = Vec.fill(NumBanks) { Reg(UInt(width = WordSize)) }
 
   for (i <- 0 until NumBanks) {
     val bank = banks(i)
-    bankReadData1(i) := bank(bankReadAddr0)
+    bankReadData2(i) := bank(bankReadAddr1)
     when (bankWriteEn(i)) {
-      bank(bankWriteAddr) := writeDataReg
+      bank(bankWriteAddr1) := writeDataReg
     }
   }
 
-  val readData1 = bankReadData1(bankReadSel1)
-  val readData2 = Reg(next = readData1)
-  io.readData := readData2
+  val readData2 = bankReadData2(bankReadSel2)
+  val readData3 = Reg(next = readData2)
+  io.readData := readData3
 }
 
 class BankedMemTester(c: BankedMem) extends Tester(c) {
