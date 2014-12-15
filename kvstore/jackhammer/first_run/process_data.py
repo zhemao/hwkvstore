5f49e2e3ab607e536a534121a98e696041a5da0d
Sagar Karandikar <karandikarsagar@gmail.com>
2014-12-14 21:15:43 -0800

    get power numbers

--- kvstore/jackhammer/first_run/process_data.py
+++ kvstore/jackhammer/first_run/process_data.py
@@ -13,6 +13,34 @@ def clean_number(num):
     return num.replace("\n", "")
 
 
+
+def powerline_to_arr(line):
+    line = line.strip()
+    line = line.split()
+    return [line[0], {"Switch": line[-5], "Int": line[-4], "Leak": line[-3], "Total": line[-2], "%": line[-1], "sub": []}]
+
+
+# processes power data
+# only handles up to 2 inner levels (4 space prefix)
+def processpower(datarray, start_index):
+    start_index += 2
+    dataret = []
+    top = powerline_to_arr(datarray[start_index])
+    i = 1
+    while True:
+        currentline = datarray[start_index+i]
+        if currentline == "1\n":
+            break
+        if currentline.startswith("  ") and not currentline.startswith("   "):
+            dataret.append(powerline_to_arr(currentline))
+        if currentline.startswith("    ") and not currentline.startswith("     "):
+            dataret[-1][1]["sub"].append(powerline_to_arr(currentline))
+        i += 1
+    top[1]["sub"] = dataret
+    return top
+
+
+
 ### round 1: get data out of the file
 def process_file_r1(filename):
     pdat = {}
@@ -24,7 +52,7 @@ def process_file_r1(filename):
 
 
     ready_for_area = False
-    
+    ready_for_power = False 
 
     arealabels = ["Combinational area", "Buf/Inv area", "Noncombinational area", "Net Interconnect area", "Total cell area", "Total area"]
 
@@ -37,6 +65,11 @@ def process_file_r1(filename):
         if "Hierarchical area distribution" in g[x]:
             print("triggered")
             ready_for_area = False
+
+        #### be sure to get the ICC power report
+        if "Report : power" in g[x] and "ICC" in g[x+4]:
+            ready_for_power = True
+
 
         # get config data
         if "class DSEConfig" in g[x]:
@@ -51,7 +84,9 @@ def process_file_r1(filename):
                     pdat[label] = clean_number(g[x].strip().split(" ")[-1])
 
         # power
-
+        if ready_for_power:
+            if "Hierarchy                              Power    Power    Power    Power    %" in g[x]:
+                pdat['power'] = processpower(g, x)
 
         # timing
 
